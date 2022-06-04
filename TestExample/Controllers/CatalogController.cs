0869f8e439139366f3673f3ebd90060de063d463
habcerga1 <habcerga1@gmail.com>
2022-06-03 17:13:09 -0700

    add: comments

--- TestExample/Controllers/CatalogController.cs
+++ TestExample/Controllers/CatalogController.cs
@@ -5,19 +5,27 @@ using TestExample.Services;
 namespace TestExample.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class CatalogController : ControllerBase
 {
     private readonly ILogger<CatalogController> _logger;
-
     public CatalogController(ILogger<CatalogController> logger)
     {
         _logger = logger;
     }
 
+    /// <summary>
+    /// Get file by name
+    /// </summary>
+    /// <param name="fileName">file name which user request in query</param>
+    /// <returns>Requested file</returns>
+    /// <remarks>
+    ///     Request example:
+    ///     Get /api/catalog?file="test.txt"
+    /// </remarks>
+    [HttpGet]
     public async Task<IActionResult> Get(string file)
     {
-        await new FileService().Request(file);
-        return Ok();
+        return Ok(await new FileService().Request(file));
     }
 }
--- TestExample/Repository/DiskDrive.cs
+++ TestExample/Repository/DiskDrive.cs
@@ -4,12 +4,20 @@ namespace TestExample.Repository;
 
 public class DiskDrive
 {
+    /// <summary>
+    /// The event that is called after reading the file
+    /// </summary>
     public event Action<string> FileReaded;
     
+    /// <summary>
+    /// Read file in Content folder async
+    /// </summary>
+    /// <param name="fileName">File name</param>
     public async Task GetAsync(string fileName)
     {
         string file = $"{AppDomain.CurrentDomain.BaseDirectory}/Content/{fileName}";
         
+        // Check if file exist
         if (System.IO.File.Exists(file))
         {
             var result = await System.IO.File.ReadAllTextAsync(file);
